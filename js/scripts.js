693f14e340792f4c2c35fd24d1ae213f3135b8de
ehuesca5 <ehuesca@vcd-ch204-24.eastern.ewu.edu>
2016-11-08 14:33:09 -0800

    v1

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
 // YOUR SCRIPTS
+// GRID VIEW
+$('#grid').click(function(){
+    // remove Only List class
+    $('.cd-gallery').removeClass('list');
+    
+    //add ONLY Grid class
+    $('.cd-gallery').addClass('grid')
+    
+    
+});
+
+//   List View
+
+$('#list').click(function(){
+    // remove Only List class
+    $('.cd-gallery').removeClass('grid');
+    
+    //add ONLY Grid class
+    $('.cd-gallery').addClass('list')
+    
+    
+});
+
